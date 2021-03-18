897ea4a09ce0d4ac6d1f2ac9120ee85648d9cf04
Talha Abbasi <talhaabbasi1997@gmail.com>
2021-03-18 20:07:53 +0500

    Add basic registration and login

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,9 +1,51 @@
-exports.register = (req, res, next) => {
-  res.send("Register route")
+const User = require("../models/User")
+
+exports.register = async (req, res, next) => {
+  const { username, email, password } = req.body
+  try {
+    const user = await User.create({
+      username,
+      email,
+      password,
+    })
+
+    res.status(201).json({
+      success: true,
+      user,
+    })
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      error: error.message,
+    })
+  }
 }
 
-exports.login = (req, res, next) => {
-  res.send("Login route")
+exports.login = async (req, res, next) => {
+  const { email, password } = req.body
+  if (!email || !password) {
+    res
+      .status(400)
+      .json({ success: false, error: "Please provide email and password" })
+  }
+  try {
+    const user = await User.findOne({ email }).select("+password")
+    if (!user) {
+      res.status(404).json({ success: false, error: "Invalid credentials" })
+    }
+    const match = await user.match(password)
+    if (!match)
+      res.status(404).json({ success: false, error: "Invalid credentials" })
+    res.status(200).json({
+      success: true,
+      token: "323eqd2edq2",
+    })
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      error: error.message,
+    })
+  }
 }
 
 exports.forgetPassword = (req, res, next) => {
--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const PORT = process.env.PORT || 5000
 require("dotenv").config({ path: "./config.env" })
 const express = require("express")
+const connectDB = require("./config/db")
+
+connectDB()
 
 const app = express()
 
@@ -8,4 +11,11 @@ app.use(express.json())
 
 app.use("/api/auth", require("./routes/auth"))
 
-app.listen(PORT, () => console.log(`Server running on port  ${PORT}`))
+const server = app.listen(PORT, () =>
+  console.log(`Server running on port  ${PORT}`)
+)
+
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Logged Error: ${err}`)
+  server.close(() => process.exit(1))
+})
